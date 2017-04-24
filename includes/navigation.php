dd4a592743b7f6b901e7edaf77e36b69ec6fce95
Rails_course_UH <ernestas.zemaitis@gmail.com>
2017-04-24 23:22:01 +0300

    adding / delete category

--- includes/navigation.php
+++ includes/navigation.php
@@ -38,10 +38,10 @@
                   
                   ?>
                    
-<!--
                     <li>
-                        <a href="#">About</a>
+                        <a href="admin">Admin</a>
                     </li>
+<!--
                     <li>
                         <a href="#">Services</a>
                     </li>
