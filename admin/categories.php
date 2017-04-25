f42089ecd4dbb4a14434b980a6f143b003b3b456
strategy <estamperija@Ernestas-Zemaitiss-MacBook-Pro.local>
2017-04-25 16:13:30 +0300

    CRUD admin categories

--- admin/categories.php
+++ admin/categories.php
@@ -27,34 +27,7 @@
                         
                         <div class="col-xs-6">
                         
-                        <?php 
-                          
-                         if(isset($_POST['submit'])){
-                           
-                          $cat_title = $_POST['cat_title'];
-                            
-                          if($cat_title == "" || empty($cat_title)){
-                            echo "This field should not be empty";
-                          } else {
-                            
-                            $query = "INSERT INTO categories(cat_title) ";
-                            $query .= "VALUE('{$cat_title}') ";
-                            
-                            $create_category_query = mysqli_query($connection, $query);
-                            
-                            if(!$create_category_query) {
-                              die('QUERY FAILED' . mysqli_error($connection));
-                            }
-                          }
-                           
-                           
-                           
-                         }
-                          
-                          
-                          
-                          
-                          ?>
+                        <?php insert_cetegories(); ?>
                          
                          
                          
@@ -67,6 +40,19 @@
                               <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                           </form>
+                          
+                          
+                        <?php //Update and include query
+                          if(isset($_GET['edit'])){
+                            $cat_id = $_GET['edit'];
+                            
+                            include "includes/update_categories.php";
+                          }
+
+                          ?>
+                          
+                          
+                          
                         </div> <!--Add Category Form-->
                         
                         <div class="col-xs-6">
@@ -81,34 +67,14 @@
                               </tr>
                             </thead>
                             <tbody>
+<!--           Find all categories query                   -->
                               
-                              
-<?php //Find all categories query
-$query = "SELECT * FROM categories";
-$select_categories = mysqli_query($connection, $query);
-
-while($row = mysqli_fetch_array($select_categories)){
-    $cat_id = $row['cat_id'];
-    $cat_title = $row['cat_title'];
-    echo "<tr>";
-    echo "<td>{$cat_id}</td>";
-    echo "<td>{$cat_title}</td>";
-    echo "<td><a href='categories.php?delete={$cat_id}'>DELETE</a></td>";
-    echo "</tr>";
-
-  }
-?> 
+<?php findAllCategories(); ?> 
 
 
  <?php //Delete QUERY
 
-  if(isset($_GET['delete'])){
-    $the_cat_id = $_GET['delete'];
-
-    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-    $delete_query = mysqli_query($connection, $query);
-    header("Location: categories.php");
-  }
+  deleteCategories();
 
   ?>
                               
