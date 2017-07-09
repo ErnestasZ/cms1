aa7b674e1977e4ca9081717a5e9858f173d13aa2
Rails_course_UH <ernestas.zemaitis@gmail.com>
2017-07-09 22:54:50 +0300

    Create user from admin

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -7,7 +7,6 @@
       <th>Lastname</th>
       <th>Email</th>
       <th>Role</th>
-      <th>Date</th>
     </tr>
   </thead>
   <tbody>
@@ -28,9 +27,9 @@
         
 
       echo "<tr>";
-        echo "<td>$comment_id</td>";
-        echo "<td>$comment_author</td>";
-        echo "<td>$comment_content</td>";
+        echo "<td>$user_id</td>";
+        echo "<td>$username</td>";
+        echo "<td>$user_firstname</td>";
       
 //        $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
 //        $select_categories_id = mysqli_query($connection,$query);
@@ -44,30 +43,29 @@
       
       
       
+        echo "<td>$user_lastname</td>";
+        echo "<td>$user_email</td>";
+        echo "<td>$user_role</td>";
       
-        echo "<td>$comment_email</td>";
-        echo "<td>$comment_status</td>";
-      
-      $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
-      $select_post_id_query = mysqli_query($connection,$query);
-      
-      while($row = mysqli_fetch_assoc($select_post_id_query)){
-        $post_id = $row['post_id'];
-        $post_title = $row['post_title'];
-        
-        echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
-        
-      }
-      
-      
+//      $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+//      $select_post_id_query = mysqli_query($connection,$query);
+//      
+//      while($row = mysqli_fetch_assoc($select_post_id_query)){
+//        $post_id = $row['post_id'];
+//        $post_title = $row['post_title'];
+//        
+//        echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+//        
+//      }
       
       
       
-        echo "<td>$comment_date</td>";
-        echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
-        echo "<td><a href='comments.php?unapprove={$comment_id}'>Unapprove</a></td>";
-        echo "<td><a href='posts.php?source=edit_post&p_id='>Edit</a></td>";
-        echo "<td><a href='comments.php?delete={$comment_id}'>Delete</a></td>";
+      
+      
+        echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";
+        echo "<td><a href='users.php?change_to_sub={$user_id}'>Subscriber</a></td>";
+        echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
+        echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
       echo "</tr>";
 
     }
@@ -82,27 +80,27 @@
 
 <?php 
 
-if(isset($_GET['approve'])){
-    $the_comment_id = $_GET['approve'];
-    $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = $the_comment_id ";
-    $approve_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
+if(isset($_GET['change_to_admin'])){
+    $the_user_id = $_GET['change_to_admin'];
+    $query = "UPDATE users SET user_role = 'admin' WHERE user_id = $the_user_id ";
+    $change_to_admin_query = mysqli_query($connection, $query);
+    header("Location: users.php");
   }
 
-if(isset($_GET['unapprove'])){
-    $the_comment_id = $_GET['unapprove'];
-    $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = $the_comment_id ";
-    $unapprove_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
+if(isset($_GET['change_to_sub'])){
+    $the_user_id = $_GET['change_to_sub'];
+    $query = "UPDATE users SET user_role = 'subscriber' WHERE user_id = $the_user_id ";
+    $change_to_sub_query = mysqli_query($connection, $query);
+    header("Location: users.php");
   }
 
 
 
   if(isset($_GET['delete'])){
-    $the_comment_id = $_GET['delete'];
-    $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id} ";
+    $the_user_id = $_GET['delete'];
+    $query = "DELETE FROM users WHERE user_id = {$the_user_id} ";
     $delete_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
+    header("Location: users.php");
   }
     
 ?>
